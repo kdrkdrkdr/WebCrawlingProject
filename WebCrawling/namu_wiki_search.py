f5299b7c4d1104b674a66b01d83ce66e542a79ee
kdr <49074597+kdrkdrkdr@users.noreply.github.com>
2019-07-04 17:39:08 +0900

    Update namu_wiki_search.py

--- WebCrawling/namu_wiki_search.py
+++ WebCrawling/namu_wiki_search.py
@@ -1,21 +1,23 @@
 from bs4 import BeautifulSoup
-from requests import get
-from os import name, system, mkdir, path, chdir, getcwd
+from requests import get, exceptions
+from os import name, system, getcwd
 from shutil import rmtree
+from pdfcrowd import HtmlToPdfClient
 
 base_url   = "https://namu.wiki"
     
 
 def Download(url, filename):
-    with open(filename, "wb") as f:
-        resp = get(url)
-        f.write(resp.content)
+    try:
+        with open(filename, "wb") as f:
+            resp = get(url)
+            f.write(resp.content)
+    except ConnectionError:
+        pass
 
-def FileWrite(filename, *lst):
-    f = open(filename, 'w', encoding='utf-8')
-    for i in lst:
-        f.write(i.text + "\n")
-    f.close()
+def MakePdfFile(docUrl, fname):
+    client = HtmlToPdfClient('kdrhacker', '9d78eaf477a691680fe410fb664e59f6')
+    client.convertUrlToFile(docUrl, fname)
 
 def ClearWindow():
     if name == 'nt':
@@ -38,7 +40,7 @@ def Banner():
 def Search():
     ClearWindow()
     try:
-        keyword = str(input('\n나무위키에서 찾을 검색어를 입력하세요. : '))
+        keyword = str(input('\n나무위키에서 찾을 문서를 입력하세요. : '))
         page    = int(input('몇쪽까지 찾아볼까요? : '))
         search_url = base_url + "/search/"
         count = 0
@@ -64,11 +66,8 @@ def Search():
 
         print("\n\n총 {}개의 결과가 검색되었습니다.".format(count))
     
-    except ValueError:
+    except (ValueError, EOFError, KeyboardInterrupt):
             print("\n다시 선택해주세요.\n")
-
-    except EOFError:
-        print("\n다시 선택해주세요.\n")
 
 
 def Document():
@@ -77,72 +76,23 @@ def Document():
         title = str(input("문서 제목을 입력해주세요. : "))
         Banner()
         w_url = base_url + "/w/" + title
-        html = get(w_url).content
-        soup = BeautifulSoup(html, 'html.parser')
 
         if title.replace(" ", "") != "":
+            filename = '{} - 나무위키.pdf'.format(title)
+            MakePdfFile(w_url, filename)
+            print("[{}\\{}] 에 '{}' 문서가 저장되었습니다.".format(getcwd(), filename, title))
 
-            wiki_document_title     = soup.find_all('span', {'class':'wiki-document-title'})
-            wiki_edit_day           = soup.find_all('p', {'class':'wiki-edit-date'})
-            wiki_category           = soup.find_all('div', {'class':'wiki-category'})
-            wiki_toc_item           = soup.find_all('span', {'class':'toc-item'})
-            wiki_heading            = soup.find_all('h2', {'class':'wiki-heading'})
-            wiki_paragraph          = soup.find_all('div', {'class':'wiki-paragraph'})
-            wiki_footnote_list      = soup.find_all('span', {'class':'footnote-list'})
-            wiki_image              = soup.find_all('img', {'class':'wiki-image'})
-
-            for h in wiki_heading:
-                print(h.text + "\n" + "="*100)
-                for p in wiki_paragraph:
-                    print(p.text)
-
-            for title in wiki_document_title:
-                dir_name = str(title.text) + '(namu.wiki)'
-
-            if '/' in dir_name:
-                dir_name = dir_name.replace('/', ' ')
-
-            if path.exists(dir_name) == True:
-                rmtree('./{}'.format(dir_name), ignore_errors=True)
-                    
-            mkdir("{}".format(dir_name))
-
-            chdir("./{}".format(dir_name))
-
-            fname = dir_name + " (문서).txt"
-
-            FileWrite(fname, 
-                *wiki_document_title,
-                *wiki_edit_day,
-                *wiki_category,
-                *wiki_toc_item,
-                *wiki_heading,
-                *wiki_paragraph,
-                *wiki_footnote_list)
-
-            img_count = 0
-            for img in wiki_image:
-                check = img.get('src')
-                for fname in wiki_document_title:
-                    if '//w.namu.la' in check:
-                        w_img_url = "https:" + check
-                        Download(w_img_url, '{}'.format(str(fname.text) + str(img_count + 1) + ".jpg").replace('/', ' '))
-                        img_count += 1
-            chdir('../')
-            
-            print("[" + getcwd() + "\\" + dir_name + "]" + " 에 나무위키 문서가 저장되었습니다.")
-
-    except KeyboardInterrupt:
+    except (ValueError, EOFError, KeyboardInterrupt):
         print("\n다시 입력해주세요.\n")
 
-Document()
+
 
 def main():
     ClearWindow()
     Banner()
     while True:
         try:
-            select = int(input("\n1) 검색어로 찾기\n2) 문서로 찾기\n3) 프로그램 종료\n\n옵션을 선택해주세요. : "))
+            select = int(input("\n1) 문서 제목 찾기\n2) 문서 다운로드 하기\n3) 프로그램 종료\n\n옵션을 선택해주세요. : "))
             if select == 1:
                 Search()
             elif select == 2:
@@ -153,11 +103,9 @@ def main():
             else:
                 print("\n다시 선택해주세요.\n")
 
-        except ValueError:
+        except (ValueError, EOFError, KeyboardInterrupt):
             print("\n다시 선택해주세요.\n")
 
-        except EOFError:
-            print("\n다시 선택해주세요.\n")
 
 if __name__ == "__main__":
     main()
