06b78fdd0a253f1548801f5a976b88e59f2db11f
kdr <49074597+kdrkdrkdr@users.noreply.github.com>
2019-08-16 13:22:56 +0900

    Update coin_market.py

--- WebCrawling/coin_market.py
+++ WebCrawling/coin_market.py
@@ -1,42 +1,45 @@
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 from csv import writer
+from time import sleep, strftime
 
+def main():
 
-hSession = HTMLSession()
-hParser = "html.parser"
+    hSession = HTMLSession()
+    hParser = "html.parser"
 
+    coinUrl = "https://coinmarketcap.com/all/views/all/"
+    coinHtml = hSession.get(coinUrl).text
+    coinSoup = BeautifulSoup(coinHtml, hParser)
 
-coinUrl = "https://coinmarketcap.com/all/views/all/"
-coinHtml = hSession.get(coinUrl).text
-coinSoup = BeautifulSoup(coinHtml, hParser)
+    coinCsv = open('{}__coinlist.csv'.format(strftime("%Y-%m-%d__%H-%M-%S")), 'w', encoding='utf-8', newline='')
+    coinWriter = writer(coinCsv)
+    coinWriter.writerow(['#', 'Name', 'Symbol', '$ Market Cap', '$ Price', '$ Circulating Supply', '$ Volume (24h)', '% 1 hour', '% 24 hours', '% 7 days'])
 
+    coinList = coinSoup.find('table', {'id':'currencies-all'}).find('tbody').find_all('tr')
 
-coinCsv = open('coinlist.csv', 'w', encoding='utf-8', newline='')
-coinWriter = writer(coinCsv)
-coinWriter.writerow(['#', 'Name', 'Symbol', '$ Market Cap', '$ Price', '$ Circulating Supply', '$ Volume (24h)', '% 1 hour', '% 24 hours', '% 7 days'])
+    for coin in coinList:
 
+        coinProperty =  coin.find_all('td')
 
-coinList = coinSoup.find('table', {'id':'currencies-all'}).find('tbody').find_all('tr')
+        coinNumber = coinProperty[0].text
+        coinName = coinProperty[1].get('data-sort')
+        coinSymbol = coinProperty[2].text
+        coinMarketCap = coinProperty[3].text.replace('$', '').replace('\n', '')
+        coinPrice = coinProperty[4].text.replace('$', '')
+        coinCirculatingSupply = coinProperty[5].text.replace('\n', '').replace('*', '')
+        coinVolume24h = coinProperty[6].text.replace('$', '').replace('\n', '')
+        coinPercent1h = coinProperty[7].text.replace('%', '')
+        coinPercent24h = coinProperty[8].text.replace('%', '')   
+        coinPercent7d = coinProperty[9].text.replace('%', '')
 
-for coin in coinList:
+        coinListBar = [coinNumber, coinName, coinSymbol, coinMarketCap, coinPrice, coinCirculatingSupply, coinVolume24h, coinPercent1h, coinPercent24h, coinPercent7d]
+        coinWriter.writerow(coinListBar)
 
-    coinProperty =  coin.find_all('td')
+    coinCsv.close()
 
-    coinNumber = coinProperty[0].text
-    coinName = coinProperty[1].get('data-sort')
-    coinSymbol = coinProperty[2].text
-    coinMarketCap = coinProperty[3].text.replace('$', '').replace('\n', '')
-    coinPrice = coinProperty[4].text.replace('$', '')
-    coinCirculatingSupply = coinProperty[5].text.replace('\n', '').replace('*', '')
-    coinVolume24h = coinProperty[6].text.replace('$', '').replace('\n', '')
-    coinPercent1h = coinProperty[7].text.replace('%', '')
-    coinPercent24h = coinProperty[8].text.replace('%', '')   
-    coinPercent7d = coinProperty[9].text.replace('%', '')
-
-
-    coinListBar = [coinNumber, coinName, coinSymbol, coinMarketCap, coinPrice, coinCirculatingSupply, coinVolume24h, coinPercent1h, coinPercent24h, coinPercent7d]
-
-    coinWriter.writerow(coinListBar)
-
-coinCsv.close()
+if __name__ == "__main__":
+    while True:
+        main()
+        print("1시간 후에 다시 가져옵니다.")
+        sleep(60*60)
